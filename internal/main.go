31021b8682bdf6ebdeed82ff37d1a7b7e8ff75f7
hrishikesh kale <hrishikesh.kale@flexiloans.com>
2020-05-25 13:13:22 +0530

    initial commit

--- internal/main.go
+++ internal/main.go
@@ -2,15 +2,32 @@ package main
 
 import (
 	"fmt"
-	"html"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
+func handleRequest() {
+	myRouter := mux.NewRouter().StrictSlash((true))
+	myRouter.HandleFunc("/kafka/publisher", pushDataToKafka)
+	myRouter.HandleFunc("/", index)
+	http.Handle("/", myRouter)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the indexPage!")
+	fmt.Println("Endpoint Hit: /")
+}
+
+func pushDataToKafka(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "pushing data to kafka...")
+	fmt.Println("Endpoint Hit: /kafka/publisher")
+}
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-	})
+	fmt.Println("Rest API v2.0 - Mux Routers")
+	handleRequest()
 	log.Println("Listening on localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
